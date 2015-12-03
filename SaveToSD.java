bbb76ef6d9cac3013cdd6abc2c033d1af58eab59
timothyhartanto <hartantotimothy@gmail.com>
2015-12-03 22:33:32 +0700

    Update SaveToSD.java

--- SaveToSD.java
+++ SaveToSD.java
@@ -1,76 +1,85 @@
-public class SaveToSD extends Activity implements OnClickListener{
-	
-	Button savePic;
-	Button saveSound;
-	EditText filename;
-	boolean isSDAvail=false, isSDWriteable =false;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.savetosd);
-		savePic = (Button) findViewById(R.id.savePicture);
-		saveSound =(Button)findViewById(R.id.saveSound);
-		filename =(EditText)findViewById(R.id.filename);
-		savePic.setOnClickListener(this);
-		saveSound.setOnClickListener(this);
-		
-		checkSDStuff();
-	}
+package com.example.proto.application;
 
-	private void checkSDStuff() {
-		// TODO Auto-generated method stub
-		String state = Environment.getExternalStorageState();
-		if(Environment.MEDIA_MOUNTED.equals(state)){
-			//write
-			isSDAvail = true;
-			isSDWriteable =true;
-		}else if(Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)){
-			//read only
-			isSDAvail = true;
-			isSDWriteable =false;
-		}else{
-			//uh-oh
-			isSDAvail = false;
-			isSDWriteable =false;
-		}
-	}
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		switch(v.getId()){
-		case R.id.savePicture:
-			if(isSDAvail && isSDWriteable){
-			File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-			String name = filename.getText().toString();
-			File file = new File(path, name + ".png");
-			
-			
-				try {
-					path.mkdirs();
-					InputStream is = getResources().openRawResource(R.drawable.exit);
-					OutputStream os;
-					os = new FileOutputStream(file);
-					byte[] data = new byte[is.available()];
-					is.read(data);
-					os.write(data);
-					is.close();
-					os.close();
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-			
-			break;
-		case R.id.saveSound:
-			
-		}
-	}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class SaveToSD extends Activity implements View.OnClickListener{
+
+    Button savePic, saveSound;
+    EditText fileName;
+    boolean isSDAvailable = false, isSDWriteable = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.savetosd);
+        savePic = (Button) findViewById(R.id.savePicture);
+        saveSound =(Button)findViewById(R.id.saveSound);
+        fileName =(EditText)findViewById(R.id.filename);
+        savePic.setOnClickListener(this);
+        saveSound.setOnClickListener(this);
+        checkSDCard();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.savePicture:
+                if(isSDAvailable && isSDWriteable){
+                    File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                    String name = fileName.getText().toString();
+                    File file = new File(path, name + ".jpg");
+
+                    // check validity of the file path
+                    try {
+                        path.mkdirs();
+                        InputStream inputStream = getResources().openRawResource(R.raw.image1);
+                        OutputStream outputStream = new FileOutputStream(file);
+
+                        byte[] data = new byte[inputStream.available()];
+                        inputStream.read(data);
+                        outputStream.write(data);
+
+                        inputStream.close();
+                        outputStream.close();
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case R.id.saveSound:
+
+                break;
+        }
+    }
+
+    private void checkSDCard() {
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            //write
+            isSDAvailable = true;
+            isSDWriteable = true;
+        }else if(Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)){
+            //read only
+            isSDAvailable = true;
+            isSDWriteable = false;
+        }else{
+            //no SD
+            isSDAvailable = false;
+            isSDWriteable = false;
+        }
+    }
+
 }
